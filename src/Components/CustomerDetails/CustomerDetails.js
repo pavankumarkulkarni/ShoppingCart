044729ba6763be464f837668642e0308d4e0c89d
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2020-09-16 21:17:20 -0400

    Implemented customer details form and session storage for cart

--- src/Components/CustomerDetails/CustomerDetails.js
+++ src/Components/CustomerDetails/CustomerDetails.js
@@ -1,29 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function CustomerDetails() {
-  const orderSubmit = (e) => {
-    console.log("order submitted");
+export default class CustomerDetails extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: "",
   };
-  return (
-    <form className="customer-details" onSubmit={orderSubmit}>
-      <p>Customer Details</p>
-      <label htmlFor="name"> Name : </label>
-      <input type="text" name="name" required placeholder="John Doe" />
-      <label htmlFor="name"> email : </label>
-      <input
-        type="email"
-        name="email"
-        required
-        placeholder=" jonh.doe@gmail.com"
-      />
-      <label htmlFor="name"> Address : </label>
-      <input
-        type="text"
-        name="address"
-        required
-        placeholder=" 123 main st some city my state 12345"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+  orderSubmit = (e) => {
+    e.preventDefault();
+    let customer = this.state;
+    this.setState({ name: "", email: "", address: "" });
+    this.props.sendCustDetails(customer);
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <form className="customer-details" onSubmit={this.orderSubmit}>
+        <p>Customer Details</p>
+        <label htmlFor="name"> Name : </label>
+        <p className="label-hint">John Doe</p>
+        <input
+          type="text"
+          name="name"
+          required
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="name"> email : </label>
+        <p className="label-hint">John.Doe@gmail.com</p>
+        <input
+          type="email"
+          name="email"
+          required
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="name"> Address : </label>
+        <p className="label-hint">123 main st some city my state 12345</p>
+        <input
+          type="text"
+          name="address"
+          required
+          value={this.state.address}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
