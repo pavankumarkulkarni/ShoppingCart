a08e5e2ae8e41764184b15654db6545e629c5d99
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2020-10-03 23:31:23 -0400

    wip1

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -10,6 +10,30 @@ export default function AboutUs() {
           pariatur reiciendis, doloremque nulla odit laudantium?
         </p>
       </header>
+      <nav>
+        <ul className={style.navbar}>
+          <li className={style.navitem}>Testimonials</li>
+          <li className={style.navitem}>Contact Us</li>
+          <li className={style.hasdropdown}>
+            <button className="btnAsLink">Theme</button>
+            <ul className={style.dropdown}>
+              <li className={style.dropdownitem}>
+                <button className="btnAsLink">Dark</button>
+              </li>
+              <li className={style.dropdownitem}>
+                <button className="btnAsLink">Light</button>
+              </li>
+              <li className={style.dropdownitem}>
+                <button className="btnAsLink">Red</button>
+              </li>
+              <li className={style.dropdownitem}>
+                <button className="btnAsLink">Green</button>
+              </li>
+            </ul>
+          </li>
+          <li className={style.navitem}>Testimonials</li>
+        </ul>
+      </nav>
       <main className={style.main}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat,
